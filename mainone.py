09fa4e0734919699c1de395a4bfda7d176dfce8d
anthonycelemen <anthony_celemen@dlsu.edu.ph>
2026-09-09 08:33:34 +0800

    Updated Files

--- mainone.py
+++ mainone.py
@@ -1,4 +1,5 @@
 from google_play_scraper import Sort, reviews, app
+import pandas as pd
 
 APP_IDS = {
     "Metrobank": "ph.com.metrobank.mcc.mbonline",
@@ -7,7 +8,27 @@ APP_IDS = {
     "UnionBank": "com.unionbankph.online"
 }
 
+all_reviews = []
 for bank_name, app_id in APP_IDS.items():
-    info = app(app_id, lang='en', country='ph')
-    print(f"{bank_name}: {info['reviews']} reviews")
+    result, continuation_token = reviews(
+        app_id,
+        lang='en',
+        country='ph',
+        sort=Sort.NEWEST,
+        count=3000
+    )
 
+    print(bank_name, len(result))
+
+    for r in result:
+        r["bank"] = bank_name
+
+    all_reviews.extend(result)
+
+print("Total collected:", len(all_reviews))
+
+df = pd.DataFrame(all_reviews)
+df.to_csv("google_play_reviews.csv", index=False)
+
+print(df.shape)
+print(df.head())
