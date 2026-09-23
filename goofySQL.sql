21032610f0078e987090d68b503ce61e4d15d55a
anthonycelemen <anthony_celemen@dlsu.edu.ph>
2026-09-23 17:31:52 +0800

    all querries

--- goofySQL.sql
+++ goofySQL.sql
@@ -7,16 +7,6 @@ CREATE TABLE reviews (
     review_date DATE,
     complaint_tags VARCHAR(200)
 );
-SHOW CREATE TABLE reviews;
-ALTER TABLE reviews CONVERT TO CHARACTER SET utf8mb4;
-LOAD DATA LOCAL INFILE 'C:/Users/ythan/Downloads/Data Analyst Practice/Topics/Jupyter/bankapp_product_analysis/df_clean2.csv'
-INTO TABLE reviews
-FIELDS TERMINATED BY ',' 
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS
-(review_id, content, score, thumbs_up_count, app_version, bank, review_date, complaint_tags);
-SET GLOBAL local_infile = 1;
 
 SELECT DISTINCT *
 FROM reviews
